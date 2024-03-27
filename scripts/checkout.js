5f3e797c3698afea8b98c0a9299b44381f3440b2
adnannextdoor <adnan@sakin.com>
2024-03-27 17:53:27 +0000

    generate data for payment summary

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,3 +1,7 @@
 import {renderOrderSummary} from "./checkout/orderSummary.js";
 
 renderOrderSummary();
+
+import {renderPaymentSummary} from "./checkout/paymentSummary.js";
+
+renderPaymentSummary();
