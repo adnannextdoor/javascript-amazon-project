cbdc805e6c98611fb4aa27f60a9cd441e3285a28
adnannextdoor <adnan@sakin.com>
2024-03-27 01:53:32 +0000

    make delivery options interactive

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import {cart, removeFromCart} from '../data/cart.js';
+import {cart, removeFromCart, updateDeliveryOption} from '../data/cart.js';
 import {products} from '../data/products.js';
 import {formatCurrency} from './utils/money.js';
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
@@ -31,8 +31,6 @@ cart.forEach((cartItem) => {
   const today = dayjs();
   const delivDate = today.add(deliveryOption.deliveryDays, 'day');
   const dateString = delivDate.format('dddd, MMMM D');
-
-  console.log(deliveryOption);
 
   cartSummaryHTML += `
     <div class="cart-item-container
@@ -91,11 +89,11 @@ function deliveryOptionsHTML(matchingProduct, cartItem) {
     
 
       const isChecked = deliveryOption.id === cartItem.deliveryOptionId;
-      console.log(deliveryOption.id);
-      console.log(cartItem.deliveryOptionId);
 
       html += `
-      <div class="delivery-option">
+      <div class="delivery-option js-delivery-option"
+        data-product-id="${matchingProduct.id}"
+        data-delivery-option-id="${deliveryOption.id}">
         <input type="radio"
           ${isChecked ? 'checked' : ''}
           class="delivery-option-input"
@@ -129,5 +127,13 @@ document.querySelectorAll('.js-delete-link')
         `.js-cart-item-container-${productId}`
       );
       container.remove();
+    });
+  });
+
+document.querySelectorAll('.js-delivery-option')
+  .forEach((element) => {
+    element.addEventListener('click', () => {
+      const {productId, deliveryOptionId} = element.dataset;
+      updateDeliveryOption(productId, deliveryOptionId);
     });
   });
